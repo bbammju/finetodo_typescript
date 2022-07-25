67e0a88b69b23f720e1ddb402e3eda6addc9b8d5
304HO <lonnie4165@gmail.com>
2022-07-25 17:19:56 +0900

    update login page

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,14 +1,33 @@
-import styled from "styled-components";
-import React from "react";
-import {Logo} from "./login.style"
+import {
+  LoginItemContainer,
+  LoginInputBox,
+  PasswordInputBox,
+  InputBoxContainer,
+  LoginButtonContainer,
+  LoginButton,
+  SignupButton,
+  KakaoLogin,
+  LogoContainer,
+} from "./login.style";
 
 const login = () => {
   return (
     <>
-
-    <div>hello</div>
-    <Logo src="/img/finetodo_logo.png"/>
+      <LoginItemContainer>
+        <LogoContainer />
+        <InputBoxContainer>
+          <LoginInputBox placeholder="아이디" />
+          <PasswordInputBox placeholder="비밀번호" type="password" />
+        </InputBoxContainer>
+        <LoginButtonContainer>
+          <LoginButton>로그인</LoginButton>
+          <div>
+            <SignupButton>회원가입</SignupButton>
+            <KakaoLogin>카카오톡 로그인</KakaoLogin>
+          </div>
+        </LoginButtonContainer>
+      </LoginItemContainer>
     </>
-  )
-}
-export default login
+  );
+};
+export default login;
