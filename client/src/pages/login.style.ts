08dcca24f88f0b756a3c5a50b6e1b940f46bb089
bbammju <stwinsilver2@gmail.com>
2022-07-25 15:57:23 +0900

    start

---
+++ client/src/pages/login.style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import React from "react";
+
+export const Logo = styled.img.attrs({alt:'logo'})`
+  width: 20vw;
+  height: 15vh;
+
+`
---
+++ client/src/pages/login.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import React from "react";
+import {Logo} from "./login.style"
+
+const login = () => {
+  return (
+    <>
+
+    <div>hello</div>
+    <Logo src="/img/finetodo_logo.png"/>
+    </>
+  )
+}
+export default login
